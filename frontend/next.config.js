9365ccb9a6183ede84013376f4de4e63afb892de
User Name <user@example.com>
2026-01-14 18:40:40 +0500

    update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/',
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://rameesha12123214-todophase02-backend.hf.space',
   },
   images: {
     unoptimized: true,
